eb20fbec9f81f66f12b04bd1c19aa0acbe65e1fb
Your Name <youremail@huskers.unl.edu>
2022-08-26 23:10:51 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Leonardo Meza
+ *         lmeza2@huskers.unl.edu
+ * Date: 2022/08/26
+ *
+ * This code prints my name.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Leonardo Meza\n");
+
+  return 0;
+}
